27133a8b5d9dad34907a358ed1d52f183ba20db9
IlyassaPutra-png <ilyasputra6071@domain.com>
2024-11-12 06:32:21 +0700

    menambahkan codingan pada file test.py

--- test.py
+++ test.py
@@ -3,9 +3,28 @@ Nama: Ilyassa Putra
 NIM: 2402741
 kELAS: 1c-rpl
 praktikum 2
-
 menambahkan kalimat 
+menambahkan sesuatu pada fitur-2
+nahh
+'''
 
+def hitung_total(angka):
+    total = sum(angka)
+    rata_rata =total/len(angka)
+    return total,rata_rata
+
+input_angka = [2,3,5,10]
+total, rata_rata = hitung_total(input_angka)
+
+print(f"Total: {input_angka} = {total}")
+print(f"Rata-rata: {total}/{len(input_angka)} = {rata_rata}")
+'''
+Nama: Ilyassa Putra
+NIM: 2402741
+kELAS: 1c-rpl
+praktikum 2
+menambahkan kalimat 
+menambahkan kalimat file fitur-1
 '''
 
 def hitung_total(angka):
